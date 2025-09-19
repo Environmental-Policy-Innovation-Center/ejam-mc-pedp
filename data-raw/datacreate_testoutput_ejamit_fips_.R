2f9a6ce2cfafd1d33110dec5a15dddb2440ba3e8
ejanalyst <ejam@ejanalysis.com>
2025-09-19 01:57:32 -0400

    resave testoutput files incl fips and shp cases, and dataset objects

--- data-raw/datacreate_testoutput_ejamit_fips_.R
+++ data-raw/datacreate_testoutput_ejamit_fips_.R
@@ -3,12 +3,42 @@ testoutput_ejamit_fips_counties <- ejamit(fips = testinput_fips_counties)
 EJAM:::metadata_add_and_use_this("testoutput_ejamit_fips_counties")
 # testoutput_ejamit_fips_counties <- metadata_add(testoutput_ejamit_fips_counties)
 # usethis::use_data(testoutput_ejamit_fips_counties, overwrite = TRUE)
-dataset_documenter("testoutput_ejamit_fips_counties", description = "This is the output of ejamit(fips = testinput_fips_counties)",
+EJAM:::dataset_documenter("testoutput_ejamit_fips_counties", description = "This is the output of ejamit(fips = testinput_fips_counties)",
                    seealso = "[ejamit()] [testdata()]")
 
 testoutput_ejamit_fips_cities <- ejamit(fips = testinput_fips_cities)
 EJAM:::metadata_add_and_use_this("testoutput_ejamit_fips_cities")
 # testoutput_ejamit_fips_cities <- metadata_add(testoutput_ejamit_fips_cities)
 # usethis::use_data(testoutput_ejamit_fips_cities, overwrite = TRUE)
-dataset_documenter("testoutput_ejamit_fips_cities", description = "This is the output of ejamit(fips = testinput_fips_cities)",
+EJAM:::dataset_documenter("testoutput_ejamit_fips_cities", description = "This is the output of ejamit(fips = testinput_fips_cities)",
                    seealso = "[ejamit()] [testdata()]")
+
+
+# save as EXCEL via ejam2excel() ####
+# if (resaving_ejam2excel) {
+  fname <- paste0("testoutput_ejam2excel_fips_cities" )
+  junk <- ejam2excel(
+     (testoutput_ejamit_fips_cities),
+    in.analysis_title = "Example of outputs of ejamit(fips= ) being formatted and saved using ejam2excel()",
+    radius_or_buffer_in_miles = 0,
+    # buffer_desc = paste0("Within ", myrad, " miles"),
+    fname = paste0("./inst/testdata/examples_of_output/", fname, ".xlsx"),
+    save_now = TRUE,
+    overwrite = TRUE,
+    launchexcel = FALSE,
+    interactive_console = FALSE
+  )
+# }
+
+# save as HTML Report via ejam2report() ####
+# if (resaving_ejam2report ) {
+  fname <- paste0("testoutput_ejam2report_fips_cities")
+  url_html <- ejam2report(
+     (testoutput_ejamit_fips_cities),
+    analysis_title = "Sample Summary Report for City FIPS",
+    launch_browser = F
+  )
+  file.copy(url_html, paste0("./inst/testdata/examples_of_output/", fname, ".html"),
+            overwrite = TRUE
+  )
+# }
