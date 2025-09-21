826e9538aa45e7ef1e492e487ad8969724118699
ejanalyst <ejam@ejanalysis.com>
2025-09-21 19:37:58 -0400

    just pkgdown

--- data-raw/pkgdown_update_background_job.R
+++ data-raw/pkgdown_update_background_job.R
@@ -1,2 +1,6 @@
-# source("./data-raw/datacreate_0_UPDATE_ALL_DOCUMENTATION_pkgdown.R")
+if (basename(getwd()) != "EJAM") {stop("must start in root of source folder")}
+source("./data-raw/datacreate_0_UPDATE_ALL_DOCUMENTATION_pkgdown.R")
+
+library(EJAM)
 EJAM:::pkgdown_update(doask = F)
+beepr::beep()
