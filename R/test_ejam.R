Environmental-Policy-Innovation-Center/ejam-mc-pedp

commit 5e5d3e3a10540e0e47d66d1ea564988493f46719

linter-based code cleanup like isTRUE(), etc.

R/app_server.R
@@ -157,7 +157,7 @@ app_server <- function(input, output, session) {
   # initial state (or if input$show_advanced_settings is changed in the adv. tab)
   observe({
     req(input$show_advanced_settings)
-    if (input$show_advanced_settings == FALSE) {
+    if (isFALSE(input$show_advanced_settings)) {
       hideTab(inputId = 'all_tabs', target = 'Advanced Settings')
       isolate({
         if (input$can_show_advanced_settings) {
@@ -1323,8 +1323,8 @@ app_server <- function(input, output, session) {
         showNotification(id = 'radius_warning', session = session,
                          duration = NULL, type = 'error', closeButton = F,
                          'Please use a numeric radius greater than 0 for analyzing points.')
-      } else if (current_slider_val[[current_upload_method()]] > 0 &
-                 disable_buttons[[current_upload_method()]] == FALSE) {
+      } else if (current_slider_val[[current_upload_method()]] > 0 &&
+                 isFALSE(disable_buttons[[current_upload_method()]])) {
         shinyjs::enable(id = 'bt_get_results')
         removeNotification(id = 'radius_warning', session = session)
       } else {
@@ -1339,7 +1339,7 @@ app_server <- function(input, output, session) {
                        duration = NULL, type = 'error', closeButton = F,
                        paste0('Too many sites provided. This web app currently caps analyses at ', input$max_pts_run,' sites.'))
     } else {
-      if (disable_buttons[[current_upload_method()]] == FALSE) {
+      if (isFALSE(disable_buttons[[current_upload_method()]])) {
         shinyjs::enable(id = 'bt_get_results')
       }
       removeNotification(id = 'max_pts_warning', session = session)
@@ -1823,10 +1823,10 @@ app_server <- function(input, output, session) {
                     # calctype_minbg = NULL,
 
                     subgroups_type      = input$subgroups_type,
-                    extra_demog         = TRUE == as.character(input$extra_demog),
-                    include_ejindexes   = TRUE == as.character(input$include_ejindexes),
-                    calculate_ratios    = TRUE == as.character(input$calculate_ratios),
-                    need_proximityscore = TRUE == as.character(input$need_proximityscore), # not relevant for FIPS
+                    extra_demog         = "TRUE" %in% as.character(input$extra_demog), # "TRUE" %in% is safe if input was a vector or NULL or logical or character, etc.
+                    include_ejindexes   = "TRUE" %in% as.character(input$include_ejindexes),
+                    calculate_ratios    = "TRUE" %in% as.character(input$calculate_ratios),
+                    need_proximityscore = "TRUE" %in% as.character(input$need_proximityscore), # not relevant for FIPS
                     # infer_sitepoints = FALSE,
                     # need_blockwt = TRUE,
 
@@ -1903,10 +1903,10 @@ app_server <- function(input, output, session) {
                       # calctype_minbg = NULL,
 
                       subgroups_type      = input$subgroups_type,
-                      extra_demog         = TRUE == as.character(input$extra_demog),
-                      include_ejindexes   = TRUE == as.character(input$include_ejindexes),
-                      calculate_ratios    = TRUE == as.character(input$calculate_ratios),
-                      need_proximityscore = TRUE == as.character(input$need_proximityscore), # not relevant for FIPS
+                      extra_demog         = "TRUE" %in% as.character(input$extra_demog),
+                      include_ejindexes   = "TRUE" %in% as.character(input$include_ejindexes),
+                      calculate_ratios    = "TRUE" %in% as.character(input$calculate_ratios),
+                      need_proximityscore = "TRUE" %in% as.character(input$need_proximityscore), # not relevant for FIPS
                       # infer_sitepoints = FALSE,
                       # need_blockwt = TRUE,
 
@@ -1962,10 +1962,10 @@ app_server <- function(input, output, session) {
                        # calctype_minbg = NULL,
 
                        subgroups_type      = input$subgroups_type,
-                       extra_demog         = TRUE == as.character(input$extra_demog),
-                       include_ejindexes   = TRUE == as.character(input$include_ejindexes),
-                       calculate_ratios    = TRUE == as.character(input$calculate_ratios),
-                       need_proximityscore = TRUE == as.character(input$need_proximityscore), # not relevant for FIPS
+                       extra_demog         = "TRUE" %in% as.character(input$extra_demog),
+                       include_ejindexes   = "TRUE" %in% as.character(input$include_ejindexes),
+                       calculate_ratios    = "TRUE" %in% as.character(input$calculate_ratios),
+                       need_proximityscore = "TRUE" %in% as.character(input$need_proximityscore), # not relevant for FIPS
                        # infer_sitepoints = FALSE,
                        # need_blockwt = TRUE,
 

R/FIPS_FUNCTIONS.R
@@ -1859,7 +1859,7 @@ fips2countyname <- function(fips, includestate = c("ST", "Statename", "")[1]) {
       substr(blockgroupstats$bgfips, 1, 5)
       )]
 
-  if (includestate == TRUE) {includestate <- "Statename"}
+  if (isTRUE(includestate)) {includestate <- "Statename"}
   if (includestate == "Statename") {addon <- fips2statename(cfips)} else {
     if (includestate == "ST") {addon <- fips2state_abbrev(cfips)} else {
       addon <- ""

R/URL_FUNCTIONS_part1.R
@@ -298,30 +298,30 @@ if (FALSE ) {
 
   # examples or tests
 
-  url_from_keylist(lat = 35, lon = -100, radius = 3.2)
-  url_from_keylist(lat = c(35,36), lon = c(-100,-99), radius = 3.14)
-
-  lat = c(35,36)
-  lon = c(-100,-99)
-  radius = 3.14
-  url_from_keylist(lat = lat, lon = lon, radius = radius)
-
-  keys = list(lat = c(35,36), lon = c(-100,-99), radius = 3.14)
-  url_from_keylist(keylist = keys)
-
-
-
-  # might want NULL to be encoded as empty parameter but that gets removed anyway
-  url_from_keylist(lat = c(35,36), lon = c(-100,-99), radius = 3.14, xyz = NULL, abc = NULL)
-
-  url_from_keylist(sitepoints=testpoints_10)
-
-
-  # bad:
-
-  url_from_keylist(keylist = list(a = 1, ))
-  url_from_keylist(a=, b=1)
-  url_from_keylist(keylist = list(a=, b=1 ))
+  # url_from_keylist(lat = 35, lon = -100, radius = 3.2)
+  # url_from_keylist(lat = c(35,36), lon = c(-100,-99), radius = 3.14)
+  #
+  # lat = c(35,36)
+  # lon = c(-100,-99)
+  # radius = 3.14
+  # url_from_keylist(lat = lat, lon = lon, radius = radius)
+  #
+  # keys = list(lat = c(35,36), lon = c(-100,-99), radius = 3.14)
+  # url_from_keylist(keylist = keys)
+  #
+  #
+  #
+  # # might want NULL to be encoded as empty parameter but that gets removed anyway
+  # url_from_keylist(lat = c(35,36), lon = c(-100,-99), radius = 3.14, xyz = NULL, abc = NULL)
+  #
+  # url_from_keylist(sitepoints=testpoints_10)
+  #
+  #
+  # # bad:
+  #
+  # url_from_keylist(keylist = list(a = 1, ))
+  # url_from_keylist(a=, b=1)
+  # url_from_keylist(keylist = list(a=, b=1 ))
 
 }
 # ########################################################### #

R/aaa_onAttach.R
@@ -36,15 +36,15 @@
   ## notloaded <- inherits(try( path.package("EJAM") , silent = TRUE), "try-error")
   notloaded_and_notinstalled <- inherits(try( find.package("EJAM") , silent = TRUE), "try-error")
   if (notloaded_and_notinstalled) {
-    cat("EJAM package must be installed or at least loaded from source already for .onAttach() to be able to use system.file(package = 'EJAM') \n")
+    packageStartupMessage("EJAM package must be installed or at least loaded from source already for .onAttach() to be able to use system.file(package = 'EJAM') \n")
     # try using local source package folders
     localpath <- file.path('./inst/global_defaults_package.R')
     if (!file.exists(localpath)) {
-      cat("Cannot find", localpath, "to create global_defaults_package object\n")
+      packageStartupMessage("Cannot find", localpath, "to create global_defaults_package object\n")
     } else {
       junk = try(source(localpath))
       if (inherits(junk, "try-error")) {
-        cat("Cannot source", localpath, "to create global_defaults_package object\n")
+        packageStartupMessage("Cannot source", localpath, "to create global_defaults_package object\n")
       }
     }
   } else {
@@ -54,7 +54,7 @@
   # It will source the file in the global environment by using local=FALSE
   # BUT, when this tries to source that .R file during .onAttach(), R has not yet attached all the .R files ?
   if (!file.exists(localpath)) {
-    cat("EJAM package is installed or loaded but cannot find file at", localpath, "\n")
+    packageStartupMessage("EJAM package is installed or loaded but cannot find file at", localpath, "\n")
   } else {
     packageStartupMessage("Trying to source a local source code copy from ", localpath, " \n")
   }
@@ -62,15 +62,15 @@
     source(localpath, local = FALSE)
     }, silent = TRUE)
   if (file.exists(localpath) && inherits(junk1, "try-error")) {
-    cat("in .onAttach() -- Unable to do
+    packageStartupMessage("in .onAttach() -- Unable to do
     source(system.file('global_defaults_package.R', package = 'EJAM')
     ")
     # localpath <- system.file('inst/global_defaults_package.R', package = 'EJAM') # system.file() does not like starting with inst/
     localpath <- file.path(dirname(system.file(package = "EJAM")), "inst", "global_defaults_package.R")
-    cat("Trying to source a local source code copy from ", localpath, " \n")
+    packageStartupMessage("Trying to source a local source code copy from ", localpath, " \n")
     junk2 = try(source(localpath), silent = TRUE)
     if (inherits(junk2, "try-error")) {
-      cat("Cannot source", localpath, "to create global_defaults_package object\n")
+      packageStartupMessage("Cannot source", localpath, "to create global_defaults_package object\n")
       warning(paste0("
     Problem in .onAttach() -- Unable to create global_defaults_package object because cannot do
        source(system.file('global_defaults_package.R', package = 'EJAM'))

R/app_ui.R
@@ -581,8 +581,8 @@ app_ui  <- function(request) {
                                                      helpText('This table shows results for each Location in the analysis. It includes location and residential population, environmental, and summary indicator information.\n
        The downloaded version includes more columns than are displayed here.'),
                                                      fluidRow(
-                                                       column(6,
-                                                              #h3(id = 'site_by_site', 'Site-by-Site Table')
+                                                       column(6
+                                                              # , h3(id = 'site_by_site', 'Site-by-Site Table')
                                                        ),
                                                        column(6,
                                                               ## button to download excel Table of Sites/Results - uses ejam2excel()

R/ejam2report.R
@@ -237,11 +237,11 @@ ejam2report <- function(ejamitout = testoutput_ejamit_10pts_1miles,
     # report_setup_temp_files() copies files to where they need to be for rendering ####
     ## returns path to .Rmd template copied to a temp folder, but
     ## tempReport is not used - report_setup_temp_files() is used for side efx
-    tempReport <- report_setup_temp_files(
+    tempReport <- report_setup_temp_files()
       # Rmd_name = 'community_report_template.Rmd', # default, for summary report
       # # Rmd_name = 'barplot_report_template.Rmd' # for single site barplot report
       # Rmd_folder = 'report/community_report/'
-    )
+
 
     # use create_filename() here like server does:
     if (!is.null(selected_location_name_react)) {

commit e4f3bf32646ab3a9d3b1bd2ead3e180605e8b0cb

FIX BUGS in server - "TRUE" vs TRUE - caused report download to fail
this bug probably was introduced recently in 5e5d3e3a10540e0e47d66d1ea564988493f46719

also added comments to test_ejam()

R/app_server.R
@@ -1813,7 +1813,7 @@ app_server <- function(input, output, session) {
                     radius = 999, # because FIPS analysis
 
                     maxradius = input$maxradius,
-                    avoidorphans = input$avoidorphans,
+                    avoidorphans = isTRUE(as.logical(input$avoidorphans)),
                     # quadtree = localtree,
 
                     # countcols      = NULL,
@@ -1823,10 +1823,10 @@ app_server <- function(input, output, session) {
                     # calctype_minbg = NULL,
 
                     subgroups_type      = input$subgroups_type,
-                    extra_demog         = "TRUE" %in% as.character(input$extra_demog), # "TRUE" %in% is safe if input was a vector or NULL or logical or character, etc.
-                    include_ejindexes   = "TRUE" %in% as.character(input$include_ejindexes),
-                    calculate_ratios    = "TRUE" %in% as.character(input$calculate_ratios),
-                    need_proximityscore = "TRUE" %in% as.character(input$need_proximityscore), # not relevant for FIPS
+                    extra_demog         = isTRUE(as.logical(input$extra_demog)),
+                    include_ejindexes   = isTRUE(as.logical(input$include_ejindexes)),
+                    calculate_ratios    = isTRUE(as.logical(input$calculate_ratios)),
+                    need_proximityscore = isTRUE(as.logical(input$need_proximityscore)), # not relevant for FIPS
                     # infer_sitepoints = FALSE,
                     # need_blockwt = TRUE,
 
@@ -1903,10 +1903,10 @@ app_server <- function(input, output, session) {
                       # calctype_minbg = NULL,
 
                       subgroups_type      = input$subgroups_type,
-                      extra_demog         = "TRUE" %in% as.character(input$extra_demog),
-                      include_ejindexes   = "TRUE" %in% as.character(input$include_ejindexes),
-                      calculate_ratios    = "TRUE" %in% as.character(input$calculate_ratios),
-                      need_proximityscore = "TRUE" %in% as.character(input$need_proximityscore), # not relevant for FIPS
+                      extra_demog         = isTRUE(as.logical(input$extra_demog)),
+                      include_ejindexes   = isTRUE(as.logical(input$include_ejindexes)),
+                      calculate_ratios    = isTRUE(as.logical(input$calculate_ratios)),
+                      need_proximityscore = isTRUE(as.logical(input$need_proximityscore)),
                       # infer_sitepoints = FALSE,
                       # need_blockwt = TRUE,
 
@@ -1962,10 +1962,10 @@ app_server <- function(input, output, session) {
                        # calctype_minbg = NULL,
 
                        subgroups_type      = input$subgroups_type,
-                       extra_demog         = "TRUE" %in% as.character(input$extra_demog),
-                       include_ejindexes   = "TRUE" %in% as.character(input$include_ejindexes),
-                       calculate_ratios    = "TRUE" %in% as.character(input$calculate_ratios),
-                       need_proximityscore = "TRUE" %in% as.character(input$need_proximityscore), # not relevant for FIPS
+                       extra_demog         = isTRUE(as.logical(input$extra_demog)),
+                       include_ejindexes   = isTRUE(as.logical(input$include_ejindexes)),
+                       calculate_ratios    = isTRUE(as.logical(input$calculate_ratios)),
+                       need_proximityscore = isTRUE(as.logical(input$need_proximityscore)), # not relevant for FIPS
                        # infer_sitepoints = FALSE,
                        # need_blockwt = TRUE,
 
@@ -2122,9 +2122,9 @@ app_server <- function(input, output, session) {
                                         analysis_title =  sanitized_analysis_title(),
                                         totalpop = popstr,
                                         locationstr = residents_within_xyz,
-                                        include_ejindexes = (input$include_ejindexes == 'TRUE'),
-                                        show_ratios_in_report = (input$show_ratios_in_report == 'TRUE'),
-                                        extratable_show_ratios_in_report = (input$extratable_show_ratios_in_report == 'TRUE'),
+                                        include_ejindexes = isTRUE(as.logical(input$include_ejindexes)),
+                                        show_ratios_in_report = isTRUE(as.logical(input$show_ratios_in_report)),
+                                        extratable_show_ratios_in_report = isTRUE(as.logical(input$extratable_show_ratios_in_report)),
                                         extratable_title = input$extratable_title, # above the table, not in the upper left cell
                                         extratable_title_top_row = input$extratable_title_top_row,
                                         extratable_list_of_sections = EJAM:::global_or_param("default_extratable_list_of_sections"),
@@ -2455,8 +2455,8 @@ app_server <- function(input, output, session) {
         shp = shp_for_report,
         analysis_title =  sanitized_analysis_title(),
         launch_browser = FALSE,
-        show_ratios_in_report = input$show_ratios_in_report,
-        extratable_show_ratios_in_report = input$extratable_show_ratios_in_report,
+        show_ratios_in_report = isTRUE(as.logical(input$show_ratios_in_report)),
+        extratable_show_ratios_in_report = isTRUE(as.logical(input$extratable_show_ratios_in_report)),
         extratable_title = input$extratable_title,
         extratable_title_top_row = input$extratable_title_top_row,
         extratable_list_of_sections = EJAM:::global_or_param("default_extratable_list_of_sections"),
@@ -2549,8 +2549,8 @@ cat("Clicked on site #", sitenumber, "for a 1-site report\n")
           shp = shp_for_report, # NULL or data_uploaded()
           analysis_title =  sanitized_analysis_title(),
           launch_browser = FALSE,
-          show_ratios_in_report = input$show_ratios_in_report,
-          extratable_show_ratios_in_report = input$extratable_show_ratios_in_report,
+          show_ratios_in_report = isTRUE(as.logical(input$show_ratios_in_report)),
+          extratable_show_ratios_in_report = isTRUE(as.logical(input$extratable_show_ratios_in_report)),
           extratable_title = input$extratable_title,
           extratable_title_top_row = input$extratable_title_top_row,
           extratable_list_of_sections = EJAM:::global_or_param("default_extratable_list_of_sections"),
@@ -2684,8 +2684,8 @@ cat("Clicked on site #", sitenumber, "for a 1-site report\n")
           shp = shp_for_report,
           analysis_title =  sanitized_analysis_title(),
           launch_browser = FALSE,
-          show_ratios_in_report = input$show_ratios_in_report,
-          extratable_show_ratios_in_report = input$extratable_show_ratios_in_report,
+          show_ratios_in_report = isTRUE(as.logical(input$show_ratios_in_report)),
+          extratable_show_ratios_in_report = isTRUE(as.logical(input$extratable_show_ratios_in_report)),
           extratable_title = input$extratable_title,
           extratable_title_top_row = input$extratable_title_top_row,
           extratable_list_of_sections = EJAM:::global_or_param("default_extratable_list_of_sections"),
@@ -2737,7 +2737,7 @@ cat("Clicked on site #", sitenumber, "for a 1-site report\n")
   # see notes on   https://exts.ggplot2.tidyverse.org/gallery/
 
   output$summ_bar_ind_out <- renderUI({
-    if ((input$include_ejindexes == "TRUE")) {
+    if (isTRUE(as.logical(input$include_ejindexes))) {
       radioButtons(inputId = 'summ_bar_ind',
                    label = h5('Indicator type'),
                    choiceNames = c('Residential Populations', 'Environmental',
@@ -2820,7 +2820,7 @@ cat("Clicked on site #", sitenumber, "for a 1-site report\n")
     req(input$include_ejindexes)
     req(input$summ_hist_data)
 
-    if ((input$include_ejindexes == "TRUE")) {
+    if (isTRUE(as.logical(input$include_ejindexes))) {
 
       ## use same root names so dropdown does not reset with other settings changing
       root_nms <-  c(names_d,

R/test_ejam.R
@@ -941,7 +941,53 @@ and all filenames listed there actually exist as in that folder called `test`.\n
 
   ## DO BASIC QUICK CHECKS, NOT UNIT TESTS   ####
   # for easy/basic case, main functions, without actually running unit tests with testthat
-
+{
+  # in_latlon = testpoints_10[1:2,]
+  # in_shp = testshapes_2
+  # in_fips = testinput_fips_mix
+  # in_fipsb = shapes_from_fips(fips_counties_from_state_abbrev("DE"))
+  # ################################################################################################### #
+  #
+  # # WEB APP CHECKS / notes
+  #
+  # #   check for report header text, logo, footer; map popups; table urls/links; plots
+  #
+  # #  ejamapp(sitepoints = in_latlon, radius = 3.14)
+  #
+  # #  ejamapp(shapefile = in_shp)
+  #
+  # #  ejamapp(fips = in_fipsb)
+  # ################################################################################################### #
+  #
+  # # R function checks
+  #
+  # #   check for report header text, logo, footer; map popups; table urls/links; plots
+  #
+  # # # LATLON
+  #
+  # out_latlon = ejamit(sitepoints = in_latlon, radius  = 3.14)
+  # ejam2map(   out_latlon)
+  # ejam2report(out_latlon)
+  # ejam2excel( out_latlon,            save_now = F, launchexcel = T)
+  # ejam2tableviewer(out_latlon)
+  #
+  # # # SHAPEFILE
+  #
+  # out_shp = ejamit(shapefile = in_shp, radius=0)
+  # ejam2map(   out_shp, shp = in_shp)
+  # ejam2report(out_shp, shp = in_shp)
+  # ejam2excel( out_shp, shp = in_shp, save_now = F, launchexcel = T)
+  # ejam2tableviewer(out_shp)
+  #
+  # # FIPS
+  #
+  # out_fips = ejamit(fips = in_fips) # in_fipsb
+  # ejam2map(   out_fips ) # in_fipsb
+  # ejam2report(out_fips ) # in_fipsb
+  # ejam2excel( out_fips,            save_now = F, launchexcel = T) # in_fipsb
+  # ejam2tableviewer(out_fips) # in_fipsb
+  # ############################################################ #
+  }
   if (!y_skipbasic) {
 
     if (y_latlon) {
@@ -952,6 +998,7 @@ and all filenames listed there actually exist as in that folder called `test`.\n
       ejam2barplot(x)
       ejam2barplot_sites(x)
       ejam2tableviewer(x)
+
       junk = ejam2excel(x, save_now = F, launchexcel = T)
 
       ejam2report(x, analysis_title = "2 point latlon example")
@@ -981,6 +1028,7 @@ and all filenames listed there actually exist as in that folder called `test`.\n
       ejam2barplot(x3)
       ejam2barplot_sites(x3)
       ejam2tableviewer(x3 , filename = file.path(tempdir(), "ejam2tableviewer_3polygon_test.html")) # should be able to pick name
+
       junk = ejam2excel(x3, save_now = F, launchexcel = T)
 
       ejam2report(x3, analysis_title = "3 polygon portland example", shp = shp)
@@ -1006,6 +1054,7 @@ and all filenames listed there actually exist as in that folder called `test`.\n
       ejam2barplot(x2)
       ejam2barplot_sites(x2)
       ejam2tableviewer(x2)
+
       junk = ejam2excel(x2, save_now = F, launchexcel = T)
 
       ejam2report(x2)