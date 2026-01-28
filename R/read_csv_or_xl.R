10b4763364e73350a9dfad381f7be96869020a74
Gabe <gabe@policyinnovation.org>
2026-01-28 16:25:30 -0500

    added a helper function to check to see if points are uploaded before advancing to help with timing on AWS

--- R/read_csv_or_xl.R
+++ R/read_csv_or_xl.R
@@ -21,9 +21,10 @@
 #' @export
 #'
 read_csv_or_xl <- function(fname = NULL, path = NULL, show_col_types = FALSE, rowsize_warn = 30 * 1000) {
-
+  
   #See if in shiny application
   in_shiny <- shiny::isRunning()
+  
   if(!in_shiny){
     if (is.null(fname)) {
       if (interactive()) {
@@ -43,20 +44,39 @@ read_csv_or_xl <- function(fname = NULL, path = NULL, show_col_types = FALSE, ro
       }
     }
   }
+  
   if (is.data.frame(fname)) {
-    # assume they accidentally provided the data not the file with the data
-    ## We maybe should warn or even disable if this is a crazy number of points (rows) ***
     return(fname)
   }
-
+  
+  # NEW: Retry wrapper for file operations
+  read_with_retry <- function(read_func, max_retries = 3) {
+    for (attempt in 1:max_retries) {
+      result <- tryCatch({
+        if (!file.exists(fname) || file.info(fname)$size == 0) {
+          stop("File not ready")
+        }
+        read_func()
+      }, error = function(e) {
+        if (attempt < max_retries) {
+          Sys.sleep(0.3 * attempt)
+          return(NULL)
+        } else {
+          stop(e$message)
+        }
+      })
+      if (!is.null(result)) return(result)
+    }
+  }
+  
   file_type <-tolower(tools::file_ext(fname))
-  ## We should disable upload of a crazy number of points - maybe check file size or do quick pre-read check of NROW() ? ***
-
+  
   filecontents <- switch(file_type,
                          csv = {
                            tryCatch({
-
-                             data <- readr::read_csv(fname, show_col_types = show_col_types) %>% as.data.frame()
+                             data <- read_with_retry(function() {
+                               readr::read_csv(fname, show_col_types = show_col_types) %>% as.data.frame()
+                             })
                              if (NROW(data) > rowsize_warn) {
                                warning("There are more than ", rowsize_warn, " rows in this dataset!")
                              }
@@ -70,23 +90,23 @@ read_csv_or_xl <- function(fname = NULL, path = NULL, show_col_types = FALSE, ro
                              NULL
                            })
                          },
-
                          xls = {
                            tryCatch({
-                             sheets <- readxl::excel_sheets(fname)
-                             if (length(sheets) > 1) {
-                               if (in_shiny) {
-                                 shiny::showNotification(
-                                   "This Excel file contains multiple sheets. Only the first sheet is processed",
-                                   type = "warning",
-                                   duration = 5
-                                 )
-                               } else {
-                                 warning("This Excel file contains multiple sheets. Only the first sheet is processed.")
+                             data <- read_with_retry(function() {
+                               sheets <- readxl::excel_sheets(fname)
+                               if (length(sheets) > 1) {
+                                 if (in_shiny) {
+                                   shiny::showNotification(
+                                     "This Excel file contains multiple sheets. Only the first sheet is processed",
+                                     type = "warning",
+                                     duration = 5
+                                   )
+                                 } else {
+                                   warning("This Excel file contains multiple sheets. Only the first sheet is processed.")
+                                 }
                                }
-                             }
-
-                             data <- readxl::read_excel(fname, sheet = 1) %>% as.data.frame()
+                               readxl::read_excel(fname, sheet = 1) %>% as.data.frame()
+                             })
                              if (NROW(data) > rowsize_warn) {
                                warning("There are more than ", rowsize_warn, " rows in this dataset!")
                              }
@@ -100,23 +120,23 @@ read_csv_or_xl <- function(fname = NULL, path = NULL, show_col_types = FALSE, ro
                              NULL
                            })
                          },
-
                          xlsx = {
                            tryCatch({
-                             sheets <- readxl::excel_sheets(fname)
-                             if (length(sheets) > 1) {
-                               if (in_shiny) {
-                                 shiny::showNotification(
-                                   "This Excel file contains multiple sheets. Only the first sheet is processed",
-                                   type = "warning",
-                                   duration = 5
-                                 )
-                               } else {
-                                 warning("This Excel file contains multiple sheets. Only the first sheet is processed.")
+                             data <- read_with_retry(function() {
+                               sheets <- readxl::excel_sheets(fname)
+                               if (length(sheets) > 1) {
+                                 if (in_shiny) {
+                                   shiny::showNotification(
+                                     "This Excel file contains multiple sheets. Only the first sheet is processed",
+                                     type = "warning",
+                                     duration = 5
+                                   )
+                                 } else {
+                                   warning("This Excel file contains multiple sheets. Only the first sheet is processed.")
+                                 }
                                }
-                             }
-
-                             data <- readxl::read_excel(fname, sheet = 1) %>% as.data.frame()
+                               readxl::read_excel(fname, sheet = 1) %>% as.data.frame()
+                             })
                              if (NROW(data) > rowsize_warn) {
                                warning("There are more than ", rowsize_warn, " rows in this dataset!")
                              }
@@ -130,11 +150,8 @@ read_csv_or_xl <- function(fname = NULL, path = NULL, show_col_types = FALSE, ro
                              NULL
                            })
                          },
-
                          stop("Invalid file type. Please upload a .csv, .xls, or .xlsx file")
   )
-
+  
   return(filecontents)
-
-
 }
